ab81fe62408061bea732a5437d50a2571d336b20
Gaboper79 <gp130579@gmail.com>
2020-09-29 19:24:02 -0300

    primer comit desde go

--- routers/login.go
+++ routers/login.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Gaboper79/gabitter/jwt"
 	"github.com/Gaboper79/gabitter/models"
 	"net/http"
+	"time"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -16,35 +17,34 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Usuario y/o contraseña invalidos"+err.Error(), 400)
 		return
 	}
-	if len(t.Email)==0{
+	if len(t.Email) == 0 {
 		http.Error(w, "El email del usuario es requerido", 400)
 		return
 	}
-	documento,existe:= bd.IntentoLogin(t.Email,t.Password)
-	if existe ==false{
-		http.Error(w, "Usuario y/o contraseña invalidos" ,400)
+	documento, existe := bd.IntentoLogin(t.Email, t.Password)
+	if existe == false {
+		http.Error(w, "Usuario y/o contraseña invalidos", 400)
 		return
 	}
-	jwtKey , err:= jwt.GeneroJWT(documento)
-	if err!=nil{
+	jwtKey, err := jwt.GeneroJWT(documento)
+	if err != nil {
 		http.Error(w, "Error al generar el Token correspondiente"+err.Error(), 400)
 		return
 	}
-	resp:=models.RespuestaLogin{
-		Token : jwtKey,
+	resp := models.RespuestaLogin{
+		Token: jwtKey,
 	}
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(resp)
 	//como grabar una cookie
-	/*
-	expirationTime := time.Now().Add(24*time.Hour)
 
-	http.SetCookie(w,*http.Cookie{
-		Name:"token",
-		Value: jwtKey,
+	expirationTime := time.Now().Add(24 * time.Hour)
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   jwtKey,
 		Expires: expirationTime,
 	})
 
-	 */
 }
